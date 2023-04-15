7fc64c2382f65c14005644bc7057705380e7d93b
niketjain1 <niketj2000@gmail.com>
2023-04-15 14:10:22 +0530

    Configured card class and added Rank and Suit enums

---
+++ src/constant/Suit.java
@@ -0,0 +1,8 @@
+package constant;
+
+public enum Suit {
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES
+}
--- src/entities/Card.java
+++ src/entities/Card.java
@@ -1,4 +1,23 @@
 package entities;
 
+import constant.Rank;
+import constant.Suit;
+
 public class Card {
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
 }
